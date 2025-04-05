5bbc01510eace635e223f3463b5307d50b657da3
Ad0mXz <adamsiller80@gmail.com>
2025-04-05 15:06:48 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+static void UpdatePresence()
+{
+    DiscordRichPresence discordPresence;
+    memset(&discordPresence, 0, sizeof(discordPresence));
+    discordPresence.state = "https://discord.gg/PxRkqMzW5h";
+    discordPresence.details = "Managing Desync Keyz";
+    discordPresence.largeImageText = "Numbani";
+    discordPresence.smallImageText = "Rogue - Level 100";
+    discordPresence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
+    discordPresence.partyMax = 5;
+    discordPresence.joinSecret = "MTI4NzM0OjFpMmhuZToxMjMxMjM= ";
+    Discord_UpdatePresence(&discordPresence);
+}
